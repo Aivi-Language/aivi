13f32bd563d491d05473a7edb9a5eec0da0dc2f5
Andreas Herd <andreas.herd@mindmine.fi>
2026-02-22 17:59:30 +0200

    lib gtk4

--- crates/aivi/src/stdlib/ui_gtk4.rs
+++ crates/aivi/src/stdlib/ui_gtk4.rs
@@ -3,8 +3,29 @@ pub const MODULE_NAME: &str = "aivi.ui.Gtk4";
 pub const SOURCE: &str = r#"
 @no_prelude
 module aivi.ui.Gtk4
-export AppId, WindowId, GtkError
-export init, appNew, windowNew, windowSetTitle, windowPresent, appRun
+export AppId, WindowId, WidgetId, BoxId, ButtonId, LabelId, EntryId, ScrollAreaId, TrayIconId, DragSourceId, DropTargetId, MenuModelId, MenuButtonId, DialogId, FileDialogId, ImageId, ListStoreId, ListViewId, TreeViewId, GestureClickId, ClipboardId, ActionId, ShortcutId, GtkError
+export init, appNew, appRun
+export windowNew, windowSetTitle, windowSetChild, windowPresent
+export widgetShow, widgetHide
+export boxNew, boxAppend
+export buttonNew, buttonSetLabel
+export labelNew, labelSetText
+export entryNew, entrySetText, entryText
+export scrollAreaNew, scrollAreaSetChild
+export trayIconNew, trayIconSetTooltip, trayIconSetVisible
+export dragSourceNew, dragSourceSetText
+export dropTargetNew, dropTargetLastText
+export menuModelNew, menuModelAppendItem, menuButtonNew, menuButtonSetMenuModel
+export dialogNew, dialogSetTitle, dialogSetChild, dialogPresent, dialogClose
+export fileDialogNew, fileDialogSelectFile
+export imageNewFromFile, imageSetFile
+export listStoreNew, listStoreAppendText, listStoreItems
+export listViewNew, listViewSetModel
+export treeViewNew, treeViewSetModel
+export gestureClickNew, gestureClickLastButton, widgetAddController
+export clipboardDefault, clipboardSetText, clipboardText
+export actionNew, actionSetEnabled, appAddAction
+export shortcutNew, widgetAddShortcut
 
-use aivi.gtk4 (AppId, WindowId, GtkError, init, appNew, windowNew, windowSetTitle, windowPresent, appRun)
+use aivi.gtk4 (AppId, WindowId, WidgetId, BoxId, ButtonId, LabelId, EntryId, ScrollAreaId, TrayIconId, DragSourceId, DropTargetId, MenuModelId, MenuButtonId, DialogId, FileDialogId, ImageId, ListStoreId, ListViewId, TreeViewId, GestureClickId, ClipboardId, ActionId, ShortcutId, GtkError, init, appNew, appRun, windowNew, windowSetTitle, windowSetChild, windowPresent, widgetShow, widgetHide, boxNew, boxAppend, buttonNew, buttonSetLabel, labelNew, labelSetText, entryNew, entrySetText, entryText, scrollAreaNew, scrollAreaSetChild, trayIconNew, trayIconSetTooltip, trayIconSetVisible, dragSourceNew, dragSourceSetText, dropTargetNew, dropTargetLastText, menuModelNew, menuModelAppendItem, menuButtonNew, menuButtonSetMenuModel, dialogNew, dialogSetTitle, dialogSetChild, dialogPresent, dialogClose, fileDialogNew, fileDialogSelectFile, imageNewFromFile, imageSetFile, listStoreNew, listStoreAppendText, listStoreItems, listViewNew, listViewSetModel, treeViewNew, treeViewSetModel, gestureClickNew, gestureClickLastButton, widgetAddController, clipboardDefault, clipboardSetText, clipboardText, actionNew, actionSetEnabled, appAddAction, shortcutNew, widgetAddShortcut)
 "#;
