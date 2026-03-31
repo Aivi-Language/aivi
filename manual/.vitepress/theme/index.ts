f31287a0882f7423f875dbc850304e389f5c4c91
Andreas Herd <mendrik76@gmail.com>
2026-03-31 22:03:39 +0300

    feat: add JetBrains Mono font support and update CSS variables

--- manual/.vitepress/theme/index.ts
+++ manual/.vitepress/theme/index.ts
@@ -1,5 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/jetbrains-mono/700.css'
 import './table-cards.css'
 import './fira-code.css'
 
